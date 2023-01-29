6602a87aaf81609800fa3c2344d4997a03ac953c
irina <irishkapos16@gmail.com>
2023-01-30 01:33:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Recognizer
+{
+    public static class ThresholdFilterTask
+    {
+        static void Main()
+        {
+            var arr = new double[,] { { 123 } };
+            Console.WriteLine(ThresholdFilter(arr,0));
+        }
+        public static double[,] ThresholdFilter(double[,] original, double whitePixelsFraction)
+        {
+            var lines = original.GetLength(0);
+            var colums = original.GetLength(1);
+            var result = new double[lines, colums];
+            var minWhitePixelsCount = (int)lines * colums * whitePixelsFraction;
+            var pixelsColorsList = new List<double>();
+            for (int x = 0; x < lines; x++)
+            {
+                for (var y = 0; y < colums; y++)
+                {
+                    pixelsColorsList.Add(original[x, y]);
+                }
+            }
+            pixelsColorsList.Sort();
+            pixelsColorsList.Reverse();
+            var maxWhitePixel = pixelsColorsList.FindIndex(x => x == minWhitePixelsCount);
+            if (original[0, 0].Equals(original[lines - 1, colums - 1])) result[0, 0] = original[0, 0] >= minWhitePixelsCount ? 1.0 : 0.0;
+            else
+            {
+                for (int x = 0; x < lines; x++)
+                {
+                    for (var y = 0; y < colums; y++)
+                    {
+                        if (original[x, y] >= maxWhitePixel) result[x, y] = 0.0;
+                        else result[x, y] = 1.0;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TextAnalysis.csproj/SentencesParserTask.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+
+            char[] delimiterChars = { '.', '!', '?', ':', ';', '(', ')' };
+
+            string[] sentences = text.ToLower().Split(delimiterChars).Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            foreach (string sentence in sentences)
+            {
+                List<string> words = new List<string>();
+
+                int startLetter = -1;
+                for (int i = 0; i < sentence.Length; i++)
+                {
+                    if (char.IsLetter(sentence[i]) || sentence[i].Equals('\''))
+                    {
+                        if (startLetter == -1)
+                            startLetter = i;
+                    }
+                    else
+                    {
+                        if (startLetter != -1)
+                            words.Add(sentence.Substring(startLetter, i - startLetter));
+
+                        startLetter = -1;
+                    }
+
+                    if (i == sentence.Length - 1 && startLetter != -1)
+                        words.Add(sentence.Substring(startLetter, sentence.Length - startLetter));
+                }
+
+                if (words.Count > 0)
+                    sentencesList.Add(words);
+            }
+
+            return sentencesList;
+        }
+    }
+}
